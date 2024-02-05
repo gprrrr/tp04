d217f05d332bb82625c3bbfc1f9be976eded211d
Guillaume Poirrier <guillaume@twelveparsecs.co>
2024-02-05 11:45:31 +0100

    impl deploy on pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ChromeDinoGame from "react-chrome-dino";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div>
+        <h1>Dino !</h1>
+        <ChromeDinoGame />
     </div>
-  );
-}
+);
 
 export default App;
