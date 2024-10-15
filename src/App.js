e5ef13f78fe67edbe28c45ce9be29c231fc4d48f
Guillaume Poirrier <guillaume@twelveparsecs.co>
2024-10-15 11:18:16 +0200

    (feat): .gitlab-ci.yml

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import React from "react";
 
 const App = () => (
     <div>
-        <h1>Dino ! attempt</h1>
-        <ChromeDinoGame />
-        <h1>this is after Merge request after MAJ</h1>
+        <ChromeDinoGame  />
     </div>
 );
 
